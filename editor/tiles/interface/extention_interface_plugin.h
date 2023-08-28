9a098a0183dd488ce34a023a872cf27ff484b17c
PatsherPhoto <143391593+PatsherPhoto@users.noreply.github.com>
2023-08-28 10:38:02 +0200

    Add files via upload

---
+++ editor/tiles/interface/extention_interface_plugin.h
@@ -0,0 +1,20 @@
+#ifndef EXTENTION_INTERFACE_PLUGIN_H
+#define EXTENTION_INTERFACE_PLUGIN_H
+
+
+/**
+ * @class ExtentionInterface
+ * @author Patsher Photo
+ * 
+*/
+
+#include <unordered_map>
+#include <unordered_set>
+
+class ExtentionInterface {
+    
+};
+
+
+
+# endif // EXTENTION_INTERFACE_PLUGIN_H
